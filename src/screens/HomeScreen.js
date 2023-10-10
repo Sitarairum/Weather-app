107e55b1e07bb76993c157bcf078f04ae1670997
Sitara Irum <saadmalikg928@gmail.com>
2023-10-10 18:31:18 +0500

    code done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -89,36 +89,36 @@ export default function HomeScreen() {
           {/* search section */}
 
           <View style={{ height: "7%" }} className="mx-4 relative z-50">
-            <KeyboardAvoidingView>
-              <View
-                className="flex-row justify-end items-center rounded-full"
-                style={{
-                  backgroundColor: showSearch
-                    ? theme.bgWhite(0.2)
-                    : "transparent",
-                }}
+            {/*<KeyboardAvoidingView>*/}
+            <View
+              className="flex-row justify-end items-center rounded-full"
+              style={{
+                backgroundColor: showSearch
+                  ? theme.bgWhite(0.2)
+                  : "transparent",
+              }}
+            >
+              {showSearch ? (
+                <TextInput
+                  onChangeText={handleTextDebounce}
+                  placeholder="Search city"
+                  placeholderTextColor={"lightgray"}
+                  className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                />
+              ) : null}
+              <TouchableOpacity
+                onPress={() => toggleSearch(!showSearch)}
+                className="rounded-full p-3 m-1"
+                style={{ backgroundColor: theme.bgWhite(0.3) }}
               >
                 {showSearch ? (
-                  <TextInput
-                    onChangeText={handleTextDebounce}
-                    placeholder="Search city"
-                    placeholderTextColor={"lightgray"}
-                    className="pl-6 h-10 pb-1 flex-1 text-base text-white"
-                  />
-                ) : null}
-                <TouchableOpacity
-                  onPress={() => toggleSearch(!showSearch)}
-                  className="rounded-full p-3 m-1"
-                  style={{ backgroundColor: theme.bgWhite(0.3) }}
-                >
-                  {showSearch ? (
-                    <XMarkIcon size="25" color="white" />
-                  ) : (
-                    <MagnifyingGlassIcon size="25" color="white" />
-                  )}
-                </TouchableOpacity>
-              </View>
-            </KeyboardAvoidingView>
+                  <XMarkIcon size="25" color="white" />
+                ) : (
+                  <MagnifyingGlassIcon size="25" color="white" />
+                )}
+              </TouchableOpacity>
+            </View>
+            {/*</KeyboardAvoidingView>*/}
             {locations.length > 0 && showSearch ? (
               <View className="absolute w-full bg-gray-300 top-16 rounded-3xl ">
                 {locations.map((loc, index) => {
@@ -147,11 +147,11 @@ export default function HomeScreen() {
           </View>
 
           {/* forecast section */}
-          <View className="mx-4 flex justify-around flex-1 mb-2">
+          <View className="mx-4 flex justify-around flex-1 mb-2 mt-3">
             {/* location */}
             <Text className="text-white text-center text-2xl font-bold">
               {location?.name},
-              <Text className="text-lg font-semibold text-gray-300">
+              <Text className="text-med font-semibold text-gray-300">
                 {location?.country}
               </Text>
             </Text>
@@ -160,12 +160,12 @@ export default function HomeScreen() {
               <Image
                 // source={{uri: 'https:'+current?.condition?.icon}}
                 source={weatherImages[current?.condition?.text || "other"]}
-                className="w-52 h-52"
+                className="w-40 h-40"
               />
             </View>
             {/* degree celcius */}
             <View className="space-y-2">
-              <Text className="text-center font-bold text-white text-6xl ml-5">
+              <Text className="text-center font-bold text-white text-4xl ml-5">
                 {current?.temp_c}&#176;
               </Text>
               <Text className="text-center text-white text-xl tracking-widest">
@@ -208,42 +208,44 @@ export default function HomeScreen() {
           {/* forecast for next days */}
 
           <View className="mb-2 space-y-3">
-            <View className="flex-row items-center mx-5 space-x-2">
-              <CalendarDaysIcon size="22" color="white" />
-              <Text className="text-white text-base">Daily forecast</Text>
-            </View>
-            <ScrollView
-              horizontal
-              contentContainerStyle={{ paddingHorizontal: 15 }}
-              showsHorizontalScrollIndicator={false}
-            >
-              {weather?.forecast?.forecastday?.map((item, index) => {
-                const date = new Date(item.date);
-                const options = { weekday: "long" };
-                let dayName = date.toLocaleDateString("en-US", options);
-                dayName = dayName.split(",")[0];
+            <KeyboardAvoidingView>
+              <View className="flex-row items-center mx-5 space-x-2">
+                <CalendarDaysIcon size="22" color="white" />
+                <Text className="text-white text-base">Daily forecast</Text>
+              </View>
+              <ScrollView
+                horizontal
+                contentContainerStyle={{ paddingHorizontal: 15 }}
+                showsHorizontalScrollIndicator={false}
+              >
+                {weather?.forecast?.forecastday?.map((item, index) => {
+                  const date = new Date(item.date);
+                  const options = { weekday: "long" };
+                  let dayName = date.toLocaleDateString("en-US", options);
+                  dayName = dayName.split(",")[0];
 
-                return (
-                  <View
-                    key={index}
-                    className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-                    style={{ backgroundColor: theme.bgWhite(0.15) }}
-                  >
-                    <Image
-                      // source={{uri: 'https:'+item?.day?.condition?.icon}}
-                      source={
-                        weatherImages[item?.day?.condition?.text || "other"]
-                      }
-                      className="w-11 h-11"
-                    />
-                    <Text className="text-white">{dayName}</Text>
-                    <Text className="text-white text-xl font-semibold">
-                      {item?.day?.avgtemp_c}&#176;
-                    </Text>
-                  </View>
-                );
-              })}
-            </ScrollView>
+                  return (
+                    <View
+                      key={index}
+                      className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+                      style={{ backgroundColor: theme.bgWhite(0.15) }}
+                    >
+                      <Image
+                        // source={{uri: 'https:'+item?.day?.condition?.icon}}
+                        source={
+                          weatherImages[item?.day?.condition?.text || "other"]
+                        }
+                        className="w-11 h-11"
+                      />
+                      <Text className="text-white">{dayName}</Text>
+                      <Text className="text-white text-xl font-semibold">
+                        {item?.day?.avgtemp_c}&#176;
+                      </Text>
+                    </View>
+                  );
+                })}
+              </ScrollView>
+            </KeyboardAvoidingView>
           </View>
         </SafeAreaView>
       )}
